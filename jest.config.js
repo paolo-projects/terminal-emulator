2b7e2b35ff09b5047ffc18ca58d489bccfd7d75f
Paolo Infante <info@paoloinfante.it>
2022-05-29 17:50:58 +0200

    fixed gh action

--- jest.config.js
+++ jest.config.js
@@ -5,6 +5,6 @@ module.exports = {
   transform: {
     ".(ts|tsx)": "<rootDir>/node_modules/ts-jest/preprocessor.js"
   },
-  testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  testRegex: "src/.*\\.(test|spec)\\.(ts|tsx|js)$",
   moduleFileExtensions: ["ts", "tsx", "js"]
 };
