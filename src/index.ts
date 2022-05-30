057ac08a4fcb3412737498fe6081ace0bcfef63d
Paolo Infante <info@paoloinfante.it>
2022-05-30 02:36:23 +0200

    removed references to web typings

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import TerminalEmulator from "./TerminalEmulator";
-import DOMOutputStream from "./OutputStream/DOMOutputStream";
 import OutputStream from "./OutputStream";
 import DefaultLocalization from "./Localization/DefaultLocalization";
 
-export {TerminalEmulator, DOMOutputStream, OutputStream, DefaultLocalization};
+export {TerminalEmulator, OutputStream, DefaultLocalization};
